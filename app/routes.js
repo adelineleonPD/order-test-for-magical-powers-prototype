44a16839a67fb91284e1f68b25a857ae707d9b20
Mike Hitchins <63401029+Mhitchins@users.noreply.github.com>
2025-11-10 15:23:18 +0000

    Added routes

--- app/routes.js
+++ app/routes.js
@@ -4,5 +4,21 @@ const express = require('express')
 const router = express.Router()
 
 // Add your routes here - above the module.exports line
+// Called when answering question about whether NHS number is known
+router.post("/magical-powers-answer", (req, res) => {
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const hasMagicalPowers = req.session.data.hasMagicalPowers;
 
+  // Check whether the variable matches a condition
+  if (hasMagicalPowers === "Yes") {
+    // Send user to a page where they'll enter their NHS number
+    res.redirect("/symptoms");
+  } else if (hasMagicalPowers === "No") {
+    // Send user to a page where they can find their NHS number
+    res.redirect("/ineligible");
+  } else {
+    // Send user back to the question page
+    res.redirect("/symptoms");
+  }
+});
 module.exports = router
