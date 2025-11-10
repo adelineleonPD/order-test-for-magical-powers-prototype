755b2df27092f04ad1960e09de0ada5dc01e227c
Mike Hitchins <63401029+Mhitchins@users.noreply.github.com>
2025-11-10 16:28:58 +0000

    added power types to check answers

--- app/routes.js
+++ app/routes.js
@@ -12,13 +12,38 @@ router.post("/magical-powers-answer", (req, res) => {
   // Check whether the variable matches a condition
   if (hasMagicalPowers === "Yes") {
     // Send user to a page where they'll enter their NHS number
-    res.redirect("/symptoms");
+    res.redirect("/power-types");
   } else if (hasMagicalPowers === "No") {
     // Send user to a page where they can find their NHS number
     res.redirect("/ineligible");
   } else {
     // Send user back to the question page
-    res.redirect("/symptoms");
+    res.redirect("/power-types");
   }
 });
+
+router.post("/power-types-answer", (req, res) => {
+  // Make a variable and give it the value from 'nhsNumberKnown'
+  const powers = req.session.data.powers;
+
+  // Check whether the variable matches a condition
+  if (
+    powers &&
+    powers.includes('Invisibility') ||
+    powers.includes('Leviation or flying') ||
+    powers.includes('Shoot lasers') ||
+    powers.includes('Jira master')
+  ){
+    res.redirect("/check-answers");
+  }
+  else if (powers && powers.includes('Other')) {
+    // Send user to a page where they can find their NHS number
+    res.redirect("/symptoms");
+  } else {
+    res.redirect("power-types")
+  }
+  
+
+});
+
 module.exports = router
